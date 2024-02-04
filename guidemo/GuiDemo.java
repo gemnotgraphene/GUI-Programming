8bdb97166536cdcdefab4374be5d59a7f5baf59e
Gemma <87027529+gemnotgraphene@users.noreply.github.com>
2024-02-04 23:02:32 +1100

    Update GuiDemo.java

--- guidemo/GuiDemo.java
+++ guidemo/GuiDemo.java
@@ -8,15 +8,10 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-/**
- * A frame that displays a multiline text, possibly with a background image
- * and with added icon images, in a DrawPanel, along with a variety of controlls.
- */
+
 public class GuiDemo extends JFrame{
 	
-	/**
-	 * The main program just creates a GuiDemo frame and makes it visible.
-	 */
+	
 	public static void main(String[] args){
 		JFrame frame = new GuiDemo();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,9 +23,7 @@ public class GuiDemo extends JFrame{
 	private TextMenu textMenu;
 	private JCheckBoxMenuItem gradientOverlayCheckbox = new JCheckBoxMenuItem("Gradient Overlay", true);
 	
-	/**
-	 * The constructor creates the frame, sizes it, and centers it horizontally on the screen.
-	 */
+	
 	public GuiDemo() {
 		
 		super("Sayings");  // Specifies the string for the title bar of the window.
@@ -43,20 +36,23 @@ public class GuiDemo extends JFrame{
 		
 		drawPanel = new DrawPanel();
 		drawPanel.getTextItem().setText(
-				"Big bugs have little bugs\n" +
-				"      Upon their backs to bite 'em,\n" +
-				"And little bugs have littler bugs,\n" +
-				"      And so it goes, ad infinitum."
+				"Dance as though no one is watching you.\n" +
+				"   Love as though you have never been hurt before.\n" +
+				"      Sing as though no one can hear you.\n" +
+				"         Live as though heaven is on earth."
 			);
+		drawPanel.getTextItem().setLineHeightMultiplier(1.5);
 		drawPanel.getTextItem().setFontSize(36);
+		drawPanel.getTextItem().setColor(Color.BLUE);
 		drawPanel.getTextItem().setJustify(TextItem.LEFT);
-		drawPanel.setBackgroundImage(Util.getImageResource("resources/images/mandelbrot.jpeg"));
+		drawPanel.setBackgroundImage(Util.getImageResource("resources/images/cats.jpeg"));
 		content.add(drawPanel, BorderLayout.CENTER);
 		
 		// Add an icon toolbar to the SOUTH position of the layout
 		
 		IconSupport iconSupport = new IconSupport(drawPanel);
 		content.add( iconSupport.createToolbar(true), BorderLayout.SOUTH );
+		content.add(makeToolbar(), BorderLayout.NORTH);
 		
 		// Create the menu bar and add it to the frame.  The TextMenu is defined by
 		// a separate class.  The other menus are created in this class.
@@ -66,6 +62,7 @@ public class GuiDemo extends JFrame{
 		textMenu = new TextMenu(drawPanel);
 		menuBar.add(textMenu );
 		menuBar.add( makeBackgroundMenu() );
+		menuBar.add(iconSupport.createMenu());
 		setJMenuBar(menuBar);
 		
 		// Set the size of the window and its position.
@@ -108,6 +105,7 @@ public class GuiDemo extends JFrame{
 	 */
 	private JMenu makeBackgroundMenu() {
 		JMenu menu = new JMenu("Background");
+		menu.add(new ChooseBackgroundAction("Cats"));
 		menu.add(new ChooseBackgroundAction("Mandelbrot"));
 		menu.add(new ChooseBackgroundAction("Earthrise"));
 		menu.add(new ChooseBackgroundAction("Sunset"));
@@ -128,6 +126,24 @@ public class GuiDemo extends JFrame{
 			}
 		});
 		return menu;
+	}
+	
+	private JToolBar makeToolbar() {
+		JToolBar toolBar = new JToolBar("Tool Bar");
+		toolBar.add(newPictureAction);
+		toolBar.add(saveImageAction);
+		toolBar.addSeparator();
+		toolBar.add(new ChooseBackgroundAction("Cats"));
+		toolBar.add(new ChooseBackgroundAction("Mandelbrot"));
+		toolBar.add(new ChooseBackgroundAction("Earthrise"));
+		toolBar.add(new ChooseBackgroundAction("Sunset"));
+		toolBar.add(new ChooseBackgroundAction("Cloud"));
+		toolBar.add(new ChooseBackgroundAction("Eagle_nebula"));
+		toolBar.addSeparator();
+		toolBar.add(new ChooseBackgroundAction("Custom..."));
+		toolBar.addSeparator();
+		toolBar.add(new ChooseBackgroundAction("Color..."));
+		return toolBar;
 	}
 	
 	private AbstractAction newPictureAction = 
@@ -190,10 +206,27 @@ public class GuiDemo extends JFrame{
     			putValue(Action.SMALL_ICON, 
     					Util.iconFromResource("resources/images/" + text.toLowerCase() + "_thumbnail.jpeg"));
     		}
-    		if (text.equals("Color..."))
+    		if (text.equals("Color...")) {
+    			BufferedImage colorIcon = new BufferedImage(32, 32, BufferedImage.TYPE_INT_RGB);
+     			Graphics g = colorIcon.createGraphics();
+     			g.setColor(Color.GRAY);
+     			g.fillRect(0, 0, 32, 32);
+    			g.setColor(Color.RED);
+    			g.fillRect(2, 2, 9, 28);
+    			g.setColor(Color.GREEN);
+    			g.fillRect(11, 2, 9, 28);
+    			g.setColor(Color.BLUE);
+    			g.fillRect(20, 2, 9, 28);
+    			g.dispose();
+    			
+    			putValue(Action.SMALL_ICON, new ImageIcon(colorIcon));
     			putValue(Action.SHORT_DESCRIPTION, "<html>Use a solid color for background<br>instead of an image.</html>");
-    		else if (text.equals("Custom..."))
+    		}
+    		else if (text.equals("Custom...")) {
+    			putValue(Action.SMALL_ICON,
+    					Util.iconFromResource("resources/action_icons/fileopen.png"));
     			putValue(Action.SHORT_DESCRIPTION, "<html>Select an image file<br>to use as the background.</html>");
+    		}
     		else
     			putValue(Action.SHORT_DESCRIPTION, "Use this image as the background.");
     			
